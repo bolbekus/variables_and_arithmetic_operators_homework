4758adb16426b60ac71fcf4ea0b80127b9363c7a
bolbekus <bolbekus@gmail.com>
2023-02-17 17:46:35 +0500

    09

--- arithmetic_op09.py
+++ arithmetic_op09.py
@@ -7,6 +7,6 @@ c = 2
 # Create a variable "d"  and assign it the value of the int 4.
 d = 4
 # Create a variable called 'answer' and assign it the expression value: a+b - cd
-answer = a + b - c / d
+answer = a + b - c * d
 
 print(answer)
